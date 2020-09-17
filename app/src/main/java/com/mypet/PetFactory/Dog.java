42ff03154f6c487cfa0e1ef5e8dc94710a87c1cd
mojfazel <fazeli.mojtaba@gmail.com>
2020-09-17 14:43:01 -0400

    adding play and feed to IPets

--- app/src/main/java/com/mypet/PetFactory/Dog.java
+++ app/src/main/java/com/mypet/PetFactory/Dog.java
@@ -1,10 +1,26 @@
 package com.mypet.PetFactory;
 
 import android.content.Context;
+import android.util.Log;
+import com.mypet.Enums.PetState;
 import com.mypet.Helpers.Message;
+import com.mypet.R;
 
 public class Dog implements IPet {
-Context _context;
+    private static final String TAG = "Dog";
+    Context _context;
+
+    @Override
+    public int play() {
+        Log.d(TAG, "Barf");
+        return R.raw.dog_barking;
+    }
+
+    @Override
+    public PetState feed() {
+        Log.d(TAG, "Dog eats bones!");
+        return PetState.Happy;
+    }
     @Override
         public void message() {
             Message.message(_context,"Congrats you got your Dog");
