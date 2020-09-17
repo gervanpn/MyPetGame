3d503a25dd7c93eea467f580686a57cd4e95fa32
burningman2k20 <burningman2k9@gmail.com>
2020-09-17 14:40:58 -0400

    fix

--- app/src/main/java/com/mypet/PetFactory/Dog.java
+++ app/src/main/java/com/mypet/PetFactory/Dog.java
@@ -1,10 +1,23 @@
 package com.mypet.PetFactory;
 
 import android.content.Context;
+
+import com.mypet.Enums.PetState;
 import com.mypet.Helpers.Message;
 
 public class Dog implements IPet {
 Context _context;
+    
+    @Override
+    public int play () {
+        return 0;
+    }
+    
+    @Override
+    public PetState feed () {
+        return null;
+    }
+    
     @Override
         public void message() {
             Message.message(_context,"Congrats you got your Dog");
