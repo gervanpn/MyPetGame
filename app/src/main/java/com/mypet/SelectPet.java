fa34154cca98c9d72c3a2b0926c3ca97d55f0b49
lalaantika <sharmal0707@gmail.com>
2020-09-17 15:49:03 -0400

    pet page sending data

--- app/src/main/java/com/mypet/SelectPet.java
+++ app/src/main/java/com/mypet/SelectPet.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import com.mypet.Enums.AnimalType;
 import com.mypet.PetFactory.IPet;
@@ -11,13 +12,18 @@ import com.mypet.PetFactory.PetTypeFactory;
 
 public class SelectPet extends AppCompatActivity {
 ImageButton cat, dog, hamster;
+EditText petName;
+String nameOfPet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_pet);
+
         cat = (ImageButton) findViewById(R.id.ImageButtonCat);
         dog = (ImageButton) findViewById(R.id.ImageButtonDog);
         hamster = (ImageButton) findViewById(R.id.ImageButtonHamster);
+        petName =(EditText) findViewById(R.id.petName);
+
         final PetTypeFactory petfactory = new PetTypeFactory();
         cat.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +31,10 @@ ImageButton cat, dog, hamster;
                 IPet petTypeCat = petfactory.getType(AnimalType.Cat);
                 petTypeCat.message(getApplicationContext());
                 Intent intent = new Intent(getApplicationContext(),PetData.class);
+                nameOfPet = petName.getText().toString();
+                intent.putExtra("name", nameOfPet);
+                intent.putExtra("Type", "Cat");
+                intent.putExtra("img", "cat_button");
                 startActivity(intent);
             }
         });
@@ -35,6 +45,10 @@ ImageButton cat, dog, hamster;
                 IPet petTypeDog = petfactory.getType(AnimalType.Dog);
                 petTypeDog.message(getApplicationContext());
                 Intent intent = new Intent(SelectPet.this,PetData.class);
+                nameOfPet = petName.getText().toString();
+                intent.putExtra("name", nameOfPet);
+                intent.putExtra("Type", "Dog");
+                intent.putExtra("img", "dog_button");
                 startActivity(intent);
             }
         });
@@ -45,6 +59,10 @@ ImageButton cat, dog, hamster;
                 IPet petTypeHamster = petfactory.getType(AnimalType.Hamster);
                 petTypeHamster.message(getApplicationContext());
                 Intent intent = new Intent(SelectPet.this,PetData.class);
+                nameOfPet = petName.getText().toString();
+                intent.putExtra("name", nameOfPet);
+                intent.putExtra("Type", "Hamster");
+                intent.putExtra("img", "hamster_btn");
                 startActivity(intent);
             }
         });
