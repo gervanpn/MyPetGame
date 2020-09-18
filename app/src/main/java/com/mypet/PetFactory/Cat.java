088c48d845ab77186acdc96b3def791946225820
burningman2k20 <burningman2k9@gmail.com>
2020-09-18 15:54:42 -0400

    Pet State working

--- app/src/main/java/com/mypet/PetFactory/Cat.java
+++ app/src/main/java/com/mypet/PetFactory/Cat.java
@@ -2,13 +2,15 @@ package com.mypet.PetFactory;
 
 import android.content.Context;
 import android.util.Log;
+
+import com.mypet.Enums.AnimalType;
 import com.mypet.Enums.PetState;
 import com.mypet.Helpers.Message;
 import com.mypet.R;
 
 public class Cat implements IPet {
     private static final String TAG = "CAT";
-
+    PetState state;
 
     @Override
     public int play() {
@@ -16,6 +18,15 @@ public class Cat implements IPet {
         return R.raw.kitten_meow;
     }
 
+    @Override
+    public void setState (PetState _state){
+        this.state = _state;
+    }
+    
+    @Override
+    public PetState getState (){
+        return this.state;
+    }
     @Override
     public PetState feed() {
         Log.d(TAG, "Cat eats mouse!");
