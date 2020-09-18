7f6d0188a0bad9e4379614fb1d2ba5398c4a854f
burningman2k20 <burningman2k9@gmail.com>
2020-09-18 14:15:55 -0400

    trying audio

--- app/src/main/java/com/mypet/PetData.java
+++ app/src/main/java/com/mypet/PetData.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import com.mypet.Enums.AnimalType;
 import com.mypet.Enums.PetState;
 import com.mypet.Helpers.DBHelper;
 import com.mypet.Helpers.Message;
+
+import java.io.IOException;
 
 public class PetData extends AppCompatActivity {
     DBHelper myDb;
@@ -21,6 +26,10 @@ public class PetData extends AppCompatActivity {
     ImageView petPicture;
     EditText petNameData, petStateData, petTypeData;
     String petName, petType, petImage;
+    
+//    MediaPlayer dog_happy_sound = new MediaPlayer();;
+//    MediaPlayer cat_happy_sound = new MediaPlayer();;
+//    MediaPlayer hamster_happy_sound = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,15 @@ public class PetData extends AppCompatActivity {
         petImage = getIntent().getExtras().getString("Img");
         int resID = getResources().getIdentifier(petImage, "drawable", this.getPackageName());
         petPicture.setImageResource(resID);
+        
+//        dog_happy_sound.create(PetData.this, R.raw.dog_barking);
+//        cat_happy_sound.create(PetData.this, R.raw.kitten_meow);
+//        try {
+//            cat_happy_sound.prepare();
+//        } catch ( IOException e ) {
+//            e.printStackTrace();
+//        }
+//        hamster_happy_sound.create(PetData.this, R.raw.hamster_squeak);
 
         ibtnList.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,8 +88,10 @@ public class PetData extends AppCompatActivity {
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-            }
+//                if (petType.equalsIgnoreCase( AnimalType.Cat.toString() )) {
+//                    cat_happy_sound.start();
+//                }
+                }
         });
 
         myDb = new DBHelper(this);
